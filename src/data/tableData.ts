a8477df4a30639007b7bfe2508ae6c810a7602e3
이재광 <yellow@wordncode.com>
2026-04-22 15:48:27 +0900

    style: 프로젝트 표준에 맞게 tableData.ts의 들여쓰기 수정

--- src/data/tableData.ts
+++ src/data/tableData.ts
@@ -1,35 +1,35 @@
 import type { TableSection } from '../types';
 
 export const tableData: TableSection[] = [
-  {
-    depth1: 'FRAME Layer',
-    data: [
-      {
-        pageTitle: 'GNB 상단',
-        id: 'LPZA101m03',
-        depth1: '',
-        depth2: '',
-        depth3: '',
-        path: 'http://m16.co.kr/bigData//dist/LPZA101m03.html',
-        progress: 40,
-        start: '2024.06.14',
-        updatedAt: '2024.06.20',
-        end: '',
-        note: '',
-      },
-      {
-        pageTitle: '',
-        id: 'LPBB102m01',
-        depth1: '',
-        depth2: '',
-        depth3: '',
-        path: 'http://m16.co.kr/bigData//dist/LPBB102m01.html',
-        progress: 0,
-        start: '',
-        updatedAt: '',
-        end: '',
-        note: '',
-      },
-    ],
-  },
+    {
+        depth1: 'FRAME Layer',
+        data: [
+            {
+                pageTitle: 'GNB 상단',
+                id: 'LPZA101m03',
+                depth1: '',
+                depth2: '',
+                depth3: '',
+                path: 'http://m16.co.kr/bigData//dist/LPZA101m03.html',
+                progress: 40,
+                start: '2024.06.14',
+                updatedAt: '2024.06.20',
+                end: '',
+                note: '',
+            },
+            {
+                pageTitle: '',
+                id: 'LPBB102m01',
+                depth1: '',
+                depth2: '',
+                depth3: '',
+                path: 'http://m16.co.kr/bigData//dist/LPBB102m01.html',
+                progress: 0,
+                start: '',
+                updatedAt: '',
+                end: '',
+                note: '',
+            },
+        ],
+    },
 ];
